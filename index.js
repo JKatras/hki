63e1ee81fd2fde1307ddd72b81c89d94afbaed1d
Jeremy Katras <jkatras@fullsail.edu>
2015-09-21 15:28:50 -0400

    no idea

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
+//$(document).ready(function() {
+jQuery(function($) {
 	var hsup = $('#home-sign-up-prompt');
 	var hsur = $('#home-sign-up-row');
 	var hfwb = $('#home-free-week-btn');
 	
-	hfwb.click({
+	hfwb.click(function(){
+	 console.log('FIRED');
 	 hfwb.toggleClass('hidden');
 	 hsur.toggleClass('hidden');
 	});
