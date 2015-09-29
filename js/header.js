6db5b7b0c07ba7863f69a80c3519f594e012dd31
Jeremy Katras <jkatras@fullsail.edu>
2015-09-29 13:20:11 -0400

    new header.js

---
+++ js/header.js
@@ -0,0 +1,14 @@
+function init() {
+	window.addEventListener('scroll', function(e) {
+		var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+			shrinkOn = 300,
+			header = document.getElementById('page-top-row');
+		if (distanceY > shrinkOn) {
+			header.addClass('smaller');
+		}else{
+			header.removeClass('smaller');
+		}
+	});
+}
+
+window.onload = init();
--- js/index.js
+++ js/index.js
@@ -1,13 +1,9 @@
-//$(document).ready(function() {
-jQuery(function($) {
+$(document).ready(function() {
+//jQuery(function($) {
 	var hsup = $('#home-sign-up-prompt');
 	var hsur = $('#home-sign-up-row');
 	var hfwb = $('#home-free-week-btn');
 	
-//	hfwb.click(function(){
-//	 hsup.toggleClass('hidden');
-//	 hsur.toggleClass('hidden');
-//	});
 	hfwb.click(function() {
 	 hsup.fadeOut(500, function() {
 	 	hsur.fadeIn(500);
