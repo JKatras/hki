5cc0a1fda2ce1860e97d957a1d2f8a63059120fb
Jeremy Katras <jkatras@fullsail.edu>
2015-10-02 16:30:21 -0400

    numerous style header changes *consolidate in style.css later*

--- js/header.js
+++ js/header.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-	
+  
+  var pageTop = $('#page-top-row');
+  
+  $(document).scroll(function() {
+  var headerScrollPos = $(this).scrollTop();
+  	if (headerScrollPos >= 250) {
+  		console.log(headerScrollPos + " is over 300");
+  		pageTop.addClass('smaller');
+  	}else {
+  		pageTop.removeClass('smaller');
+  	}
+  });
+  
 });
