034d99c85569f938c7fe085524b808fb60c48bdb
Jeremy Katras <jkatras@fullsail.edu>
2015-10-04 21:37:20 -0400

    member header nav sticky

--- js/header.js
+++ js/header.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
   
   var pageTop = $('#page-top-row');
+  var memNav = $('#mem-nav');
   
   $(document).scroll(function() {
   var headerScrollPos = $(this).scrollTop();
   	if (headerScrollPos >= 250) {
-  		console.log(headerScrollPos + " is over 300");
+//  		console.log(headerScrollPos + " is over 300");
   		pageTop.addClass('smaller');
   	}else {
   		pageTop.removeClass('smaller');
+  	}
+  	if (headerScrollPos >=90) {
+  		memNav.addClass('sticky');
+  	}else {
+  		memNav.removeClass('sticky');
   	}
   });
   
