3113d87927f2fc564b465a8314de7a9cacb9f236
Jeremy Katras <jkatras@fullsail.edu>
2015-10-04 23:11:08 -0400

    index head styling/js

--- js/index.js
+++ js/index.js
@@ -13,24 +13,34 @@ $(document).ready(function() {
 	 return false;
 	});
 	
-//homepage animate GIFs on scroll
+//	homepage add sticky header after scrolling past video
 
-	var gifLoadPos = $('#snoopy-gif').offset().top;
-	var notLoaded = true;
-  $(document).scroll(function(){
-      var scrollPosition = $(this).scrollTop();
-      console.log(scrollPosition, 'doc pos');
-      console.log(gifLoadPos, 'gif pos');
-      
-      if(gifLoadPos - scrollPosition <= 300 && notLoaded)
-      {
-      	$('#snoopy-gif').attr('src', '/assets/img/placeholder/snoopy.gif');
-      	$('#fall-leaves-gif').attr('src', '/assets/img/placeholder/fall_leaves.gif');
-      	$('#corgi-gif').attr('src', '/assets/img/placeholder/corgi_leaves.gif');
-      	$('#stream-gif').attr('src', '/assets/img/placeholder/stream.gif');
-      	notLoaded = false;
-      }
-  });
+	var homeHeader = $('.home-header').attr('class');
+	var homeSocialNav = $('.home-social-nav').attr('class');
+	$(document).scroll(function() {
+		var scrollPosition = $(this).scrollTop();
+		if (scrollPosition >= 800) {
+			homeHeader.addClass('fixed');
+			homeSocialNav.addClass('fixed');
+		}
+	console.log(scrollPosition);
+	});
+	
+	//homepage animate GIFs on scroll
+	
+//	var gifLoadPos = $('#snoopy-gif').offset().top;
+//	var notLoaded = true;
+//  $(document).scroll(function(){
+//      var scrollPosition = $(this).scrollTop();    
+//      if(gifLoadPos - scrollPosition <= 300 && notLoaded)
+//      {
+//      	$('#snoopy-gif').attr('src', '/assets/img/placeholder/snoopy.gif');
+//      	$('#fall-leaves-gif').attr('src', '/assets/img/placeholder/fall_leaves.gif');
+//      	$('#corgi-gif').attr('src', '/assets/img/placeholder/corgi_leaves.gif');
+//      	$('#stream-gif').attr('src', '/assets/img/placeholder/stream.gif');
+//      	notLoaded = false;
+//      }
+//  });
  
 	
 });
