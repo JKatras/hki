01f9464577ad36b4e30ac0a423635f8f7e3f52a3
Jeremy Katras <jkatras@fullsail.edu>
2015-10-05 21:36:46 -0400

    final mods pre-launch

--- js/index.js
+++ js/index.js
@@ -15,32 +15,45 @@ $(document).ready(function() {
 	
 //	homepage add sticky header after scrolling past video
 
-	var homeHeader = $('.home-header').attr('class');
-	var homeSocialNav = $('.home-social-nav').attr('class');
-	$(document).scroll(function() {
-		var scrollPosition = $(this).scrollTop();
-		if (scrollPosition >= 800) {
+	var homeHeader = $('.home-header');
+	var homeSocialNav = $('.home-social-nav');
+	var homeHeaderWrap = $('#home-header-wrap');
+	$(window).on('scroll', function() {
+		var scrollPosition = $(window).scrollTop();
+		if(scrollPosition >= $('#video-overlay').height()-20) {
+//		if (scrollPosition >= 800) {
 			homeHeader.addClass('fixed');
 			homeSocialNav.addClass('fixed');
+  		homeHeaderWrap.addClass('shown');
+//  		.fadeTo(500, 1, function(){
+//   			homeHeaderWrap.css("visibility", "visible");   
+//			});
 		}
-	console.log(scrollPosition);
+		else {
+			homeHeader.removeClass('fixed');
+			homeSocialNav.removeClass('fixed');
+			homeHeaderWrap.removeClass('shown');
+//			.fadeTo(500, 0, function(){
+//   			homeHeaderWrap.css("visibility", "hidden");   
+//			});
+		}
 	});
 	
 	//homepage animate GIFs on scroll
 	
-//	var gifLoadPos = $('#snoopy-gif').offset().top;
-//	var notLoaded = true;
-//  $(document).scroll(function(){
-//      var scrollPosition = $(this).scrollTop();    
-//      if(gifLoadPos - scrollPosition <= 300 && notLoaded)
-//      {
-//      	$('#snoopy-gif').attr('src', '/assets/img/placeholder/snoopy.gif');
-//      	$('#fall-leaves-gif').attr('src', '/assets/img/placeholder/fall_leaves.gif');
-//      	$('#corgi-gif').attr('src', '/assets/img/placeholder/corgi_leaves.gif');
-//      	$('#stream-gif').attr('src', '/assets/img/placeholder/stream.gif');
-//      	notLoaded = false;
-//      }
-//  });
+	var gifLoadPos = $('#snoopy-gif').offset().top;
+	var notLoaded = true;
+  $(document).scroll(function(){
+      var scrollPosition = $(this).scrollTop();    
+      if(gifLoadPos - scrollPosition <= 300 && notLoaded)
+      {
+      	$('#snoopy-gif').attr('src', '/assets/img/placeholder/snoopy.gif');
+      	$('#fall-leaves-gif').attr('src', '/assets/img/placeholder/fall_leaves.gif');
+      	$('#corgi-gif').attr('src', '/assets/img/placeholder/corgi_leaves.gif');
+      	$('#stream-gif').attr('src', '/assets/img/placeholder/stream.gif');
+      	notLoaded = false;
+      }
+  });
  
 	
 });
